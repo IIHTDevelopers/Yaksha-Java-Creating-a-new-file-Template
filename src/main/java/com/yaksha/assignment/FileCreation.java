cc824875a2274b201dd3d6fcf1966d4a3d9ba04a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-27 18:47:46 +0530

    Update FileCreation.java

--- src/main/java/com/yaksha/assignment/FileCreation.java
+++ src/main/java/com/yaksha/assignment/FileCreation.java
@@ -7,18 +7,7 @@ public class FileCreation {
 
 	public static void main(String[] args) {
 		// **Create a new file**
-		File file = new File("newFile.txt");
-
-		try {
-			// **Check if the file already exists, if not create a new file**
-			if (file.createNewFile()) {
-				System.out.println("File created: " + file.getName());
-			} else {
-				System.out.println("File already exists.");
-			}
-		} catch (IOException e) {
-			System.out.println("An error occurred while creating the file.");
-			e.printStackTrace();
-		}
+		
+		// **Check if the file already exists, if not create a new file**
 	}
 }
